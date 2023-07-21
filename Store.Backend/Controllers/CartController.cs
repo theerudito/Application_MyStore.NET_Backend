4fba340bbd099148f27017727ba9b7fc903ef881
theerudito <erudito.tv@gmail.com>
2023-07-21 15:36:20 -0500

    v5

--- Store.Backend/Controllers/CartController.cs
+++ Store.Backend/Controllers/CartController.cs
@@ -4,5 +4,12 @@ namespace Store.Backend.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class CartController : ControllerBase { }
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GET_CART()
+        {
+            return Ok(new { message = "Hola que tal como vas" });
+        }
+    }
 }
